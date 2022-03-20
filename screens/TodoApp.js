41cfba3d9c81928e7cac5a72a37d4e2e56ffbbf9
Stephen Mhuli <stevemshible@gmail.com>
2022-03-20 16:47:07 +0300

    add a new lib

---
+++ screens/TodoApp.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+
+import { Constants } from 'expo-constants';
+
+//imports 
+import ButtonIcon from '../components/buttonIcon';
+
+import Spacer from '../components/Spacer';
+
+import { Title, Card, Paragraph, Button, TextInput } from 'react-native-paper';
+
+import { FontAwaresome as Icon } from '@expo/vector-icons';
+
+im
